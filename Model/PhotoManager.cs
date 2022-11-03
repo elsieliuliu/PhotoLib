9dd47f342e9dda76b0e96ceb6d675546a1f091d8
elsieliuliu <elsieliu.uw@gmail.com>
2022-11-03 16:17:52 -0700

    finally made it work

--- Model/PhotoManager.cs
+++ Model/PhotoManager.cs
@@ -11,7 +11,7 @@ namespace PhotoLib.Model
     {
         public static void GetAllPhotos(ObservableCollection<Photo> photos) //tracking changes constantly
         {
-            var allPhotos = GetPhotos(); //allPhotos points at a list of photos, sounds is pointing to an empty
+            var allPhotos = GetPhotos(); //allPhotos points at a list of photos,
             photos.Clear();
             allPhotos.ForEach(photo => photos.Add(photo));
             
@@ -24,17 +24,41 @@ namespace PhotoLib.Model
             var filteredPhotos = allPhotos.Where(photo => photo.Album == album).ToList();
             filteredPhotos.ForEach(photo => photos.Add(photo));
         }
+
+        public static void GetOnePhotoByClick(ObservableCollection<Photo> photos2,
+            PhotoName name)
+        {
+            var allPhotos2 = GetPhotos2();
+            photos2.Clear();
+            var onePhoto = allPhotos2.Where(photo => photo.Name == name).ToList();
+            onePhoto.ForEach(photo => photos2.Add(photo));
+
+
+
+        }
         private static List<Photo> GetPhotos() // a method 
         {
             var photos = new List<Photo>(); // use this class defined in Photo.cs as a type
-            photos.Add(new Photo("kitten_1", PhotoAlbum.Album_1));
-            photos.Add(new Photo("kitten_2", PhotoAlbum.Album_1));
-            photos.Add(new Photo("puppy_1", PhotoAlbum.Album_2));
-            photos.Add(new Photo("puppy_2", PhotoAlbum.Album_2));
-            photos.Add(new Photo("bunny_1", PhotoAlbum.Album_3));
-            photos.Add(new Photo("bunny_2", PhotoAlbum.Album_3));
+            photos.Add(new Photo(PhotoName.kitten_1, PhotoAlbum.Album_1));
+            photos.Add(new Photo(PhotoName.kitten_2, PhotoAlbum.Album_1));
+            photos.Add(new Photo(PhotoName.puppy_1, PhotoAlbum.Album_2));
+            photos.Add(new Photo(PhotoName.puppy_2, PhotoAlbum.Album_2));
+            photos.Add(new Photo(PhotoName.bunny_1, PhotoAlbum.Album_3));
+            photos.Add(new Photo(PhotoName.bunny_2, PhotoAlbum.Album_3));
 
             return photos;
+        }
+        private static List<Photo> GetPhotos2() 
+        {
+            var photos2 = new List<Photo>(); 
+            photos2.Add(new Photo(PhotoName.kitten_1, PhotoAlbum.Album_1));
+            photos2.Add(new Photo(PhotoName.kitten_2, PhotoAlbum.Album_1));
+            photos2.Add(new Photo(PhotoName.puppy_1, PhotoAlbum.Album_2));
+            photos2.Add(new Photo(PhotoName.puppy_2, PhotoAlbum.Album_2));
+            photos2.Add(new Photo(PhotoName.bunny_1, PhotoAlbum.Album_3));
+            photos2.Add(new Photo(PhotoName.bunny_2, PhotoAlbum.Album_3));
+
+            return photos2;
         }
     }
 }
